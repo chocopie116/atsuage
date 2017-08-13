f2c8ede3620ffd54fbef3b9da79aed6aadf4065d
chocopie116 <chocopie116@gmail.com>
2017-08-13 12:10:35 +0900

    ..

--- functions/slackbot/main.go
+++ functions/slackbot/main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-	//"encoding/json"
-
 	"github.com/apex/go-apex"
+	"log"
 	"encoding/json"
 )
 
 type message struct {
-	Value string `json:"message"`
+	Token string `json:"token"`
+	TeamId string `json:"team_id"`
+	TeamDomain string `json:"team_domain"`
+	ChannelId string `json:"channel_id"`
+	ChannelName string `json:"channel_name"`
+	Timestamp string `json:"timestamp"`
+	UserId string `json:"user_id"`
+	UserName string `json:"user_name"`
+	Text string `json:"text"`
+	TriggerWord string `json:"trigger_word"`
 }
 
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-		//var m message
+		var m message
+		if err := json.Unmarshal(event, &m); err != nil {
+			return nil, err
+		}
+		log.Printf("%s\n", m)
 
-		//if err := json.Unmarshal(event, &m); err != nil {
-		//	return nil, err
-		//}
-
-		return map[string]string{"text": "test"}, nil
+		return map[string]string{"text": m.Text}, nil
 	})
 }
