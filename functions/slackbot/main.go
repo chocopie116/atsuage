de0096b89a531912c9ee65be89eb8e70d4e86499
chocopie116 <chocopie116@gmail.com>
2017-08-13 13:06:24 +0900

    add

--- functions/slackbot/main.go
+++ functions/slackbot/main.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 )
 
-type SlackChatMessage struct {
+type slackChatMessage struct {
 	Token string `json:"token"`
 	TeamId string `json:"team_id"`
 	TeamDomain string `json:"team_domain"`
@@ -24,7 +24,7 @@ func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
 		log.Printf("%s\n", event)
 
-		var m SlackChatMessage
+		var m slackChatMessage
 		if err := json.Unmarshal(event, &m); err != nil {
 			return nil, err
 		}
